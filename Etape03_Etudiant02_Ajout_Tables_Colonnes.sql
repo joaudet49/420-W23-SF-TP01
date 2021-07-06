5f3030bc21cba992c6878bb2fa8a5f68c79da487
SimonB-G <simonbgirard@gmail.com>
2021-07-06 15:15:08 -0400

    Étape 3 et légères modifs dans les 2 premières

--- Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,35 @@
 /* 
  Travail pratique 1
- Nom de la première personne
- --> Nom de la deuxième personne
+ Jonathan Audet
+ --> Simon Bergeron-Girard
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS typeusager;
+CREATE TABLE typeusager (
+id_typeusager INT PRIMARY KEY,
+nom_statut VARCHAR(45)
+)ENGINE=InnoDB;
+
+INSERT INTO typeusager
+VALUES (1,'Professeur'),(2,'Employé'),(3,'Étudiant');
+
+ALTER TABLE usager
+ADD COLUMN id_Type_usager INTEGER null;
+
+ALTER TABLE logiciel
+ADD CONSTRAINT nom_logiciel_unique UNIQUE (nom_logiciel);
+
+ALTER TABLE editeur
+ADD CONSTRAINT nom_editeur_unique UNIQUE (nom_editeur);
+
+ALTER TABLE logiciel
+MODIFY nom_logiciel VARCHAR(45) NOT NULL;
+
+ALTER TABLE editeur
+MODIFY nom_editeur VARCHAR(45) NOT NULL;

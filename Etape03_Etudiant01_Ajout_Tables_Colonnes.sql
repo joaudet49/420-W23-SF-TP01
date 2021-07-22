4ac46120b6a2b22696e126cdf718ba0c10053ca6
joaudet49 <joaudet4@gmail.com>
2021-07-22 15:21:19 -0400

    Etape 3 fait pour Jonathan Audet

--- Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,41 @@
 /* 
- Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+Travail pratique 1
+ Jonathan Audet
+ --> Simon Bergeron-Girard
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS typeperipherique;
+CREATE TABLE typeperipherique (
+id_typeperipherique INT PRIMARY KEY,
+nom_statut_peripherique VARCHAR(45)
+)ENGINE=InnoDB;
+
+INSERT INTO typeperipherique
+VALUES (1,'Imprimante'),(2,'Souris'),(3,'Clavier'),(4,'Écran');
+
+ALTER TABLE peripherique
+ADD COLUMN id_type_peripherique INT,
+ADD FOREIGN KEY (id_type_peripherique) REFERENCES typeperipherique(id_typeperipherique);
+
+ALTER TABLE local
+ADD COLUMN nb_bureau VARCHAR(45),
+ADD COLUMN nb_prise_ethernet VARCHAR(45),
+ADD COLUMN imprimante INT;
+
+ALTER TABLE local
+ADD CONSTRAINT nom_local_unique UNIQUE (nom_local);
+
+ALTER TABLE fabricant
+ADD CONSTRAINT nom_fabricant_unique UNIQUE (nom_fabricant);
+
+ALTER TABLE local
+MODIFY nom_local VARCHAR(45) NOT NULL;
+
+ALTER TABLE fabricant
+MODIFY nom_fabricant VARCHAR(45) NOT NULL;

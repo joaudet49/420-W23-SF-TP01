c1f749d1b0d5d30d1fbe56424febbc818e27a295
joaudet49 <joaudet4@gmail.com>
2021-08-01 12:50:17 -0400

    Etape 4 fait pour Jonathan Audet

--- Etape04_Etudiant01_MAJ_Donnees.sql
+++ Etape04_Etudiant01_MAJ_Donnees.sql
@@ -1,12 +1,46 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+Jonathan Audet
+ --> Simon Bergeron-Girard
  
- Date  JJ-MM-AAAA                                             
+ Date  31-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 4 : Mise à jour des données (Clause UPDATE)  --
 -- -----------------------------------------------------
+UPDATE local
+SET
+	nb_bureau = '16',
+    nb_prise_ethernet = '16',
+    imprimante = 674
+WHERE nom_local = 'P-210';
+
+UPDATE local
+SET
+	nb_bureau = '27',
+    nb_prise_ethernet = '27',
+    imprimante = 676
+WHERE nom_local = 'P-308';
+
+UPDATE local
+SET
+	nb_bureau = '27',
+    nb_prise_ethernet = '27',
+    imprimante = 677
+WHERE nom_local = 'P-309';
+
+UPDATE usager
+SET id_type_usager = 1
+WHERE id_usager BETWEEN 1 AND 9;
+
+UPDATE usager
+SET id_type_usager = 2
+WHERE id_usager BETWEEN 100 AND 999;
+
+UPDATE usager
+SET id_type_usager = 3
+WHERE id_usager>=1000 ;
+
+
